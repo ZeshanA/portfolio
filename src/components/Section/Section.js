f33a2a449235d617070c0efe496415712eca37c0
Zeshan Amjad <zeshan@zesh.me>
2019-01-19 02:43:20 +0000

    Remove FontAwesome Pro from devDependencies

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -6,7 +6,7 @@ import styles from "./Section.module.scss";
 const Section = props => {
   const { title, children, className } = props;
   return (
-    <section className={`${className} ${styles.section}`}>
+    <section className={`${className ? className : ""} ${styles.section}`}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.content}>{children}</div>
     </section>
