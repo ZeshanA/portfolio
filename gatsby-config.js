cd26d8e8973988d3f541adb9959b8e69a3143d3b
Zeshan Amjad <zeshan@zesh.me>
2019-01-22 22:05:36 +0000

    Create copy of default Gatsby html.js to inject additional metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Zeshan Amjad | Front-end Developer"
+    title: "Zeshan Amjad | Front-end Developer",
+    description:
+      "Zeshan Amjad is a front-end developer with experience in creating beautiful, usable web interfaces."
   },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`]
 };
