f6f54aa0a7228444388eb1a398bcbab55df92800
Zeshan Amjad <zeshan@zesh.me>
2020-05-19 13:28:33 +0100

    Update title to reflect full stack role

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Zeshan Amjad | Front-end Developer",
+    title: "Zeshan Amjad | Full Stack Developer",
     description:
-      "Zeshan Amjad is a front-end developer with experience in creating beautiful, usable web interfaces."
+      "Zeshan Amjad is a full stack developer with experience in creating beautiful, usable web interfaces."
   },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`]
 };
